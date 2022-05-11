2c61d751da8974bf11b2c23e43290363edcc8057
Wankyu Jung <skyship36@gmail.com>
2022-05-11 17:05:19 +0900

    initial commit

---
+++ src/main/java/com/mightyotter/learnershigh/LearnersHighApplication.java
@@ -0,0 +1,13 @@
+package com.mightyotter.learnershigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnersHighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnersHighApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mightyotter/learnershigh/LearnersHighApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mightyotter.learnershigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnersHighApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
