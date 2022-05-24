2e11be94036cc4667bb6cd55a35a885eba01193c
Wankyu Jung <skyship36@gmail.com>
2022-05-24 16:03:35 +0900

    refactor(main): remove @Query annotation from JPA method 'findByEmail'

--- src/main/java/com/mightyotter/learnershigh/domain/member/dao/MemberRepository.java
+++ src/main/java/com/mightyotter/learnershigh/domain/member/dao/MemberRepository.java
@@ -1,14 +1,12 @@
 package com.mightyotter.learnershigh.domain.member.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 // import com.mightyotter.learnershigh.domain.model.Member; // Member 클래스를 Model로 분리 보관 해야할까?
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	@Query(value = "SELECT EMAIL FROM TBL_MEMBER WHERE EMAIL = :email LIMIT 1")
-	String findByEmail(@Param("email") String email);
+	List<Member> findByEmail(String email);
 }
