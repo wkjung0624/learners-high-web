1179257394acf4e94d217bac7d6757487a39c5a8
Wankyu Jung <skyship36@gmail.com>
2022-05-24 16:14:31 +0900

    feat(main): Add 'MemberSaveRequestDto' DTO class to pass user registration data

---
+++ src/main/java/com/mightyotter/learnershigh/domain/member/dto/MemberSaveRequestDto.java
@@ -0,0 +1,40 @@
+package com.mightyotter.learnershigh.domain.member.dto;
+
+import com.mightyotter.learnershigh.domain.member.dao.Member;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+public class MemberSaveRequestDto {
+	@NotNull
+	private final String userId;
+	@NotNull
+	private final String userPw;
+
+	@NotNull
+	private final String nickName;
+
+	@Email
+	@Getter
+	@NotNull
+	private final String email;
+
+	@Builder
+	public MemberSaveRequestDto(String userId, String userPw, String nickName, String email){
+		this.userId = userId;
+		this.userPw = userPw;
+		this.nickName = nickName;
+		this.email = email;
+	}
+
+	public Member toEntity(){
+		return Member.builder()
+			.userId(userId)
+			.userPw(userPw)
+			.nickName(nickName)
+			.email(email)
+			.verifiedEmail(false)
+			.build();
+	}
+}
