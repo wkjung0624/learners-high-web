bd22f0606955388e00148039a6c42d060322a12e
Wankyu Jung <skyship36@gmail.com>
2022-05-30 15:18:24 +0900

    refactor: delete DTO classes
    Delete Dto Classes
    - MemberCreateRequestDto
    - MemberLoginRequestDto
    - MemberLoginResponseDto
    - MemberUpdateRequestDto
    - MemberDeleteRequestDto

--- src/main/java/com/mightyotter/learnershigh/domain/member/application/MemberService.java
+++ src/main/java/com/mightyotter/learnershigh/domain/member/application/MemberService.java
@@ -2,10 +2,11 @@ package com.mightyotter.learnershigh.domain.member.application;
 
 import com.mightyotter.learnershigh.domain.member.dao.Member;
 import com.mightyotter.learnershigh.domain.member.dao.MemberRepository;
-import com.mightyotter.learnershigh.domain.member.dto.MemberCreateRequestDto;
 
-import com.mightyotter.learnershigh.domain.member.dto.MemberLoginRequestDto;
-import java.util.List;
+import com.mightyotter.learnershigh.domain.member.dto.MemberDeleteRequestDto;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,19 +16,86 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 	private final MemberRepository memberRepository;
 
-	@Transactional
-	public Long save(MemberCreateRequestDto memberSaveRequestDto){
-		return memberRepository.save(memberSaveRequestDto.toEntity()).getId();
+	@Transactional(rollbackFor = Exception.class)
+	public Map<String, String> save(String userId, String userPw, String nickName, String email){
+
+		Map<String, String> response = new HashMap<>();
+		if(findOneByUserId(userId) == null &&
+			findOneByEmail(userPw) == null){
+
+			memberRepository.save(Member.builder()
+				.userId(userId)
+				.userPw(userPw)
+				.nickName(nickName)
+				.email(email)
+				.verifiedEmail(false)
+				.build());
+			response.put("result","ok");
+			return response;
+		}
+
+		response.put("result","false");
+		return response;
+	}
+	@Transactional(rollbackFor = Exception.class)
+	public boolean delete(MemberDeleteRequestDto memberDeleteRequestDto){
+
+		Optional<Member> member = memberRepository.findOneByUserIdAndUserPw(
+			memberDeleteRequestDto.getUserId(), memberDeleteRequestDto.getUserPw());
+
+		if (member.isPresent()){
+			memberRepository.delete(member.get());
+			return true;
+		}
+		// TODO: 아이디와 비밀번호 외에 유저의 "로그인 인증값" 도 같이 제출되어야함
+		return false;
 	}
 
-	public List<Member> findByUserIdAndUserPw(MemberLoginRequestDto memberLoginRequestDto) {
-		return memberRepository.findByUserIdAndUserPw(memberLoginRequestDto.getUserId(),
-			memberLoginRequestDto.getUserPw());
+	@Transactional(rollbackFor = Exception.class)
+	public boolean changePassword(String userId, String userPw){
+
+		Optional<Member> member = memberRepository.findOneByUserId(userId);
+
+		if(member.isPresent()){
+			member.get().setUserPw(userPw);
+			return true;
+		}
+		return false;
 	}
-	public List<Member> findByEmail(String email){
-		return memberRepository.findByEmail(email);
+
+	public Member findOneByEmail(String email){
+		return memberRepository
+			.findOneByEmail(email)
+			.orElse(null);
 	}
-	public List<Member> findByUserId(String userId){
-		return memberRepository.findByUserId(userId);
+	public Member findOneByUserId(String userId) {
+		return memberRepository
+			.findOneByUserId(userId)
+			.orElse(null);
+	}
+
+	public Member getUser(String userId, String userPw) {
+
+		Member member;
+
+		if(false){ // if (rediscasch.get("userId") != null) {
+			/* Redis 에서 기존에 저장한 findOneByUserId 메서드의 결과값을 가져와 선비교 후 DB 질의하기
+			* member = RedisCache.get("userId");
+			* */
+		} else {
+			// 레디스에 멤버 캐쉬 데이터 저장 과정 추가
+			member = findOneByUserId(userId);
+		}
+
+		if(member.getUserPw().equals(userPw)) {
+			return member;
+		}
+		return null;
+	}
+
+	public Member findOneByUserPw(String userId, String userPw) {
+		return memberRepository
+			.findOneByUserIdAndUserPw(userId, userPw)
+			.orElse(null);
 	}
 }
