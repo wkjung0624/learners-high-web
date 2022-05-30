676eaaa6b4ec7a54c14a72334b99acd827d8ce76
Wankyu Jung <skyship36@gmail.com>
2022-05-31 02:04:08 +0900

    feat(main): create Article Service class

---
+++ src/main/java/com/mightyotter/learnershigh/domain/article/application/ArticleService.java
@@ -0,0 +1,12 @@
+package com.mightyotter.learnershigh.domain.article.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+
+	public Long save(String categoryId, String authorId, String title, String content, String hashtag,
+		String thumbnailUrlPath, String mediaDataJson) {
+		return null;
+	}
+}
