1c1a3c52ee51311f825f853fc6a3ff301ad1589f
Wankyu Jung <skyship36@gmail.com>
2022-05-30 09:45:18 +0900

    feat(main): add 'delete' JPA method for member withdrawal

--- src/main/java/com/mightyotter/learnershigh/domain/member/dao/MemberRepository.java
+++ src/main/java/com/mightyotter/learnershigh/domain/member/dao/MemberRepository.java
@@ -1,6 +1,6 @@
 package com.mightyotter.learnershigh.domain.member.dao;
 
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 // import com.mightyotter.learnershigh.domain.model.Member; // Member 클래스를 Model로 분리 보관 해야할까?
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	// findOneById
-	List<Member> findByEmail(String email);
-	List<Member> findByUserId(String userId);
-	List<Member> findByUserIdAndUserPw(String userId, String userPw);
+	Optional<Member> findOneByEmail(String email);
+	Optional<Member> findOneByUserId(String userId);
+	Optional<Member> findOneByUserIdAndUserPw(String userId, String userPw);
+	void delete(Member member);
 }
