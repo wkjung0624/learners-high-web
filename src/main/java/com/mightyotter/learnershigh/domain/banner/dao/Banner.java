14e0514fe9a954c3d49e90ba66c52a968317cee5
Wankyu Jung <skyship36@gmail.com>
2022-06-09 13:52:24 +0900

    fix(banner): add constructor to Banner entity

--- src/main/java/com/mightyotter/learnershigh/domain/banner/dao/Banner.java
+++ src/main/java/com/mightyotter/learnershigh/domain/banner/dao/Banner.java
@@ -1,17 +1,22 @@
 package com.mightyotter.learnershigh.domain.banner.dao;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Banner {
 	@Id
@@ -29,6 +34,13 @@ public class Banner {
 	// 배너 클릭시 연결될 하이퍼링크
 	@Column(columnDefinition = "TEXT", nullable = false)
 	private String bannerHypertextLink;
+
+	// 배너 개제 예정일
+//	@Column
+//	private Date reservedDate;
+	// 배너 자동 비공개 전환 예정일
+//	@Column
+//	private Date expireDate;
 
 	@Column
 	private boolean visible;
