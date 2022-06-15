35c6116ac192c4ee83de9ad0bb51908e904d87df
Wankyu Jung <skyship36@gmail.com>
2022-06-15 20:12:20 +0900

    feat(club): add DTO class to update club information

---
+++ src/main/java/com/mightyotter/learnershigh/domain/club/dto/ClubUpdateRequestDto.java
@@ -0,0 +1,27 @@
+package com.mightyotter.learnershigh.domain.club.dto;
+
+import com.mightyotter.learnershigh.domain.club.dao.Club;
+import lombok.Setter;
+
+@Setter
+public class ClubUpdateRequestDto {
+	private String clubName;
+	private String password;
+	private String mainImageUrlPath;
+	private String description;
+	private int maximumUserCount; // 클럽 활동성별 유저 제한?
+	private boolean visible;
+
+	public Club toEntity(){
+		return Club.builder()
+			.clubName(clubName)
+			.password(password)
+			.mainImageUrlPath(mainImageUrlPath)
+			.description(description)
+			.maximumUserCount(maximumUserCount)
+			.visible(visible)
+			.deleted(false)
+			.build();
+	}
+	// 추후에 클럽 관련된 테이블을 여러개로 분산하여서 저장? 그로 인한 이점은 무엇일지, 검색-색인 속도가 빨라질까?
+}
