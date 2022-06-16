62c1b6288ca997615f5813051f5f4770d47b6bd3
Wankyu Jung <skyship36@gmail.com>
2022-06-17 00:06:57 +0900

    feat(util): add utility to upload files sent by clients

---
+++ src/main/java/com/mightyotter/learnershigh/global/util/FileUploadUtil.java
@@ -0,0 +1,28 @@
+package com.mightyotter.learnershigh.global.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import org.springframework.web.multipart.MultipartFile;
+
+// FileUploadUtil source from >> https://www.codejava.net/frameworks/spring-boot/spring-boot-file-upload-tutorial
+public class FileUploadUtil {
+	public static void saveFile(String uploadDir, String fileName, MultipartFile file) throws IOException {
+
+		Path uploadPath = Paths.get(uploadDir);
+
+		if (!Files.exists(uploadPath)) {
+			Files.createDirectories(uploadPath);
+		}
+
+		try (InputStream inputStream = file.getInputStream()) {
+			Path filePath = uploadPath.resolve(fileName);
+			Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException ioe) {
+			throw new IOException("Could not save image file: " + fileName, ioe);
+		}
+	}
+}
