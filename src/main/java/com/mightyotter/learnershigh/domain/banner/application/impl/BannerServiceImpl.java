338254820c483b91c3fc49861efcd667aef7ab2d
Wankyu Jung <skyship36@gmail.com>
2022-06-17 01:42:23 +0900

    feat(banner): add file upload service to 'addBanner' method

--- src/main/java/com/mightyotter/learnershigh/domain/banner/application/impl/BannerServiceImpl.java
+++ src/main/java/com/mightyotter/learnershigh/domain/banner/application/impl/BannerServiceImpl.java
@@ -1,12 +1,19 @@
 package com.mightyotter.learnershigh.domain.banner.application.impl;
 
+import com.mightyotter.learnershigh.domain.banner.dao.BannerType;
+import com.mightyotter.learnershigh.global.util.FileUploadUtil;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.mightyotter.learnershigh.domain.banner.application.BannerService;
 import com.mightyotter.learnershigh.domain.banner.dao.Banner;
 import com.mightyotter.learnershigh.domain.banner.dao.BannerRepository;
 import com.mightyotter.learnershigh.domain.banner.dto.BannerSaveDto;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
@@ -20,9 +27,24 @@ public class BannerServiceImpl implements BannerService {
 		return bannerRepository.findByType(type);
 	}
 	@Override
-	public Long addBanners(String type, BannerSaveDto bannerSaveDto){
-		bannerSaveDto.setType(type);
-		return bannerRepository.save(bannerSaveDto.toEntity()).getBannerId();
+	public Long addBanners(String type, BannerSaveDto bannerSaveDto) throws IllegalArgumentException, IOException {
+
+		bannerSaveDto.setType(BannerType.valueOf(type.toUpperCase()).getValue()); // IllegalArgumentException point
+
+		MultipartFile bannerImageFile = bannerSaveDto.getBannerImageFile();
+
+		if(bannerImageFile == null) throw new FileNotFoundException("Banner image file not found");
+
+		String uploadDir = "tmp/banner/thumbnail/";
+		String fileName = StringUtils.cleanPath(
+			Objects.requireNonNull(bannerImageFile.getOriginalFilename()));
+
+		FileUploadUtil.saveFile(uploadDir,fileName,bannerImageFile);
+
+		Banner banner = bannerSaveDto.toEntity();
+		banner.setBannerImageSourceUrl(uploadDir + fileName);
+
+		return bannerRepository.save(banner).getBannerId();
 	}
 	@Override
 	public void changeBannerVisible(Long bannerId, boolean flag){
