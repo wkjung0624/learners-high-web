5870671f8a8328808776f11eb872c56983e5d868
Wankyu Jung <skyship36@gmail.com>
2022-06-17 01:57:51 +0900

    feat(banner): add throws to exception handling (+IllegalArgumentException, IOException)

--- src/main/java/com/mightyotter/learnershigh/domain/banner/application/BannerService.java
+++ src/main/java/com/mightyotter/learnershigh/domain/banner/application/BannerService.java
@@ -2,10 +2,12 @@ package com.mightyotter.learnershigh.domain.banner.application;
 
 import com.mightyotter.learnershigh.domain.banner.dao.Banner;
 import com.mightyotter.learnershigh.domain.banner.dto.BannerSaveDto;
+import java.io.IOException;
 import java.util.List;
+
 public interface BannerService {
 	List<Banner> getAllBanners(String type);
-	Long addBanners(String type, BannerSaveDto bannerSaveDto);
+	Long addBanners(String type, BannerSaveDto bannerSaveDto) throws IllegalArgumentException, IOException;
 	void changeBannerVisible(Long bannerId, boolean flag);
 	void deleteBanners(Long bannerId);
 }
