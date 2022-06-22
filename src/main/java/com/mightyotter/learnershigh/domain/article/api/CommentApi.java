0636d49a8c50ad67c78286fe97f38c433275a811
Wankyu Jung <skyship36@gmail.com>
2022-06-22 19:47:33 +0900

    feat(comment): change API return type to default response structure
    * Affected methods *
    
    - createComment
    - getCommentList
    - editComment
    - deleteComment

--- src/main/java/com/mightyotter/learnershigh/domain/article/api/CommentApi.java
+++ src/main/java/com/mightyotter/learnershigh/domain/article/api/CommentApi.java
@@ -3,8 +3,12 @@ package com.mightyotter.learnershigh.domain.article.api;
 import com.mightyotter.learnershigh.domain.article.application.CommentService;
 import com.mightyotter.learnershigh.domain.article.domain.Comment;
 import com.mightyotter.learnershigh.domain.article.dto.CommentSaveDto;
+import com.mightyotter.learnershigh.global.common.response.StandardResponseBody;
+import com.mightyotter.learnershigh.global.common.response.data.DataLayer;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,23 +23,57 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class CommentApi {
 	private final CommentService commentService;
-
 	@PostMapping("/article/{articleId}/comment")
-	public void createComment(@PathVariable Long articleId, @RequestBody CommentSaveDto commentSaveDto){
+	public ResponseEntity<StandardResponseBody> createComment(@PathVariable Long articleId, @RequestBody CommentSaveDto commentSaveDto){
 		commentService.createComment(articleId,commentSaveDto);
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(
+				StandardResponseBody.builder()
+					.data(DataLayer.builder()
+						.status(true)
+						.build())
+					.build());
 	}
 	@GetMapping("/article/{articleId}/comment")
-	public List<Comment> getComments(@PathVariable Long articleId){
-		return commentService.getComments(articleId);
+	public ResponseEntity<StandardResponseBody> getCommentList(@PathVariable Long articleId){
+		List<Comment> commentList = commentService.getComments(articleId);
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(
+				StandardResponseBody.builder()
+					.data(DataLayer.builder()
+						.items(commentList)
+						.build())
+					.build());
 	}
 	// 수정과 삭제는 CommentId 만 있으면 되는데 일관된 주소를 쓰기 위해서 굳이 ArticleId 를 써야할까?
+	// TODO: true 로 공통된 결과를 출력하는 것은 하나만 있어도 되는거 아닐까? 너무 반복됨
 	@PostMapping("/article/{articleId}/comment/{commentId}/edit")
-	public Comment editComment(@PathVariable Long articleId, @PathVariable Long commentId, @RequestBody String content){
-		return commentService.editComment(commentId, content);
+	public ResponseEntity<StandardResponseBody> editComment(@PathVariable Long articleId, @PathVariable Long commentId, @RequestBody String content){
+		commentService.editComment(commentId, content);
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(
+				StandardResponseBody.builder()
+					.data(DataLayer.builder()
+						.status(true)
+						.build())
+					.build());
 	}
 	// 수정과 삭제는 CommentId 만 있으면 되는데 일관된 주소를 쓰기 위해서 굳이 ArticleId 를 써야할까?
 	@PostMapping("/article/{articleId}/comment/{commentId}/delete")
-	public void deleteComment(@PathVariable Long articleId, @PathVariable Long commentId){
+	public ResponseEntity<StandardResponseBody> deleteComment(@PathVariable Long articleId, @PathVariable Long commentId){
 		commentService.deleteComment(commentId);
+
+		return ResponseEntity
+			.status(HttpStatus.NO_CONTENT)
+			.body(
+				StandardResponseBody.builder()
+					.data(DataLayer.builder()
+						.status(true)
+						.build())
+					.build());
 	}
 }
