f4d758a44d55cff00a5cfea524441c7f0ddf4ee7
Wankyu Jung <skyship36@gmail.com>
2022-07-04 14:27:39 +0900

    feat(club): add '@NoArgsConstructor' into 'ClubMember' entity

--- src/main/java/com/learnershigh/api/domain/club/domain/ClubMember.java
+++ src/main/java/com/learnershigh/api/domain/club/domain/ClubMember.java
@@ -1,21 +1,26 @@
 package com.learnershigh.api.domain.club.domain;
 
+import com.learnershigh.api.domain.club.model.PermissionLevel;
 import com.learnershigh.api.domain.member.domain.Member;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-@Entity
 public class ClubMember {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,18 +37,11 @@ public class ClubMember {
 	//@JoinColumn(name = "MEMBER_ID")
 	private Member member;
 
+	// 최초 생성자 ID 또는 마스터 권한 위임자(Member 클래스로 매핑 필요)
+	@Column(nullable = false)
+	private PermissionLevel permissionLevel;
+
 	// 해당 클럽에서 퇴장당했을 때 재입장 제한을 위한 값
 	@Column(nullable = false)
 	private boolean banned;
 }
-
-//	@Column(nullable = false)
-//	private String authority;
-
-
-	// 클럽 고유번호
-	// 클럽명
-	// 클럽 관리자(생성자)
-	// 클럽 매니저
-	// 클럽 유저
-
