b7068d40a2983aa001d975372a11cbb93299ecd2
Wankyu Jung <skyship36@gmail.com>
2022-07-07 15:46:05 +0900

    feat(main): add '@EnableGlobalMethodSecurity' to manage API calls with authentication
    set securedEnabled to true

--- src/main/java/com/learnershigh/api/Application.java
+++ src/main/java/com/learnershigh/api/Application.java
@@ -3,10 +3,12 @@ package com.learnershigh.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableJpaAuditing
 @EnableRedisHttpSession
+@EnableGlobalMethodSecurity(securedEnabled = true) // 도메인 별 일부 혹은 모든 Api가 특정 권한(ROLE_*)을 가진 유저만 사용하게끔 옵션 설정
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
